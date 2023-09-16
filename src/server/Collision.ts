984b55eb953e6c1cf196e1b897407f443438bc64
Metee <viixxivvimm@gmail.com>
2023-09-17 04:22:23 +0700

    fix: Player directory

--- src/server/Collision.ts
+++ src/server/Collision.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from "./utils/uuid";
 import { Vector3 } from "@censor1337/cfx-api/server";
 import { Collision as CollisionBase } from "../shared/Collision";
-import { Player } from "./Player";
+import { Player } from "./entities/Player";
 import * as cfx from "@censor1337/cfx-api/server";
 
 export abstract class Collision extends CollisionBase {
