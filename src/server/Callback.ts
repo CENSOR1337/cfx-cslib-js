7e21e5a3a59cf02e3f580ef4c862ecc1dea99d1d
Metee <viixxivvimm@gmail.com>
2023-10-10 15:57:30 +0700

    refactor: cslib callback using source as player source

--- src/server/Callback.ts
+++ src/server/Callback.ts
@@ -1,24 +1,22 @@
-import { Player } from "./entities/Player";
 import { randomUUID } from "./utils/uuid";
 import { Event } from "@censor1337/cfx-api/server";
 import { Callback as CallbackShared } from "../shared/Callback";
 
 export class Callback extends CallbackShared {
-	public static emit<T>(eventName: string, player: Player, ...args: any[]): Promise<T> {
+	public static emit<T>(eventName: string, source: number, ...args: any[]): Promise<T> {
 		const cbId = randomUUID();
 		const promise = new Promise<T>((resolve, reject) => {
 			Event.onceClient(cbId, (source: number, data: any) => {
 				resolve(data as T);
 			});
 		});
-		player.emit(`${this.clientNamespace}:${eventName}`, cbId, ...args);
+		Event.emit(`${this.clientNamespace}:${eventName}`, source, cbId, ...args);
 		return promise;
 	}
 
-	public static register(eventName: string, handler: (player: Player, ...args: any[]) => void): Event {
+	public static register(eventName: string, handler: (source: number, ...args: any[]) => void): Event {
 		return Event.onClient(`${this.serverNamespace}:${eventName}`, (source: number, cbId: string, ...args: any[]) => {
-			const player = Player.fromSource(source);
-			player.emit(cbId, handler(player, ...args));
+			Event.emit(cbId, handler(source, ...args));
 		});
 	}
 }
